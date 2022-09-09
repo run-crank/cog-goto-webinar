6fab354ee302bf9b083fd71bc9d7e737be8ee133
Angelito Salud <asalud@you-source.com>
2022-09-09 18:42:41 +0800

    Implemented ordered data on field check steps

--- src/steps/registrant/registrant-field-equals.ts
+++ src/steps/registrant/registrant-field-equals.ts
@@ -77,19 +77,19 @@ export class RegistrantFieldEqualsStep extends BaseStep implements StepInterface
       // Using regex, put quotes on the registrantKey to get full key when parsed
       data = data.replace(/([\[:])?(\d+)([,\}\]])/g, '$1\"$2\"$3');
       data = JSON.parse(data);
+      const records = this.createRecords(data, stepData['__stepOrder']);
 
       if (data && data.hasOwnProperty(field)) {
         const result = this.assert(operator, data[field], expectedValue, field);
-
-        return result.valid ? this.pass(result.message, [], [this.createRecord(data)])
-          : this.fail(result.message, [], [this.createRecord(data)]);
+        return result.valid ? this.pass(result.message, [], records)
+          : this.fail(result.message, [], records);
 
       } else {
         if (data && !data.hasOwnProperty(field)) {
           return this.fail(
             'Found the registrant with key %s, but there was no %s field.',
             [registrantKey, field],
-            [this.createRecord(data)],
+            records,
           );
         } else {
           return this.fail("Couldn't find a registrant associated with %s", [
@@ -113,8 +113,13 @@ export class RegistrantFieldEqualsStep extends BaseStep implements StepInterface
     }
   }
 
-  createRecord(registrant: Record<string, any>) {
-    return this.keyValue('registrant', 'Checked Registrant', registrant);
+  createRecords(registrant: Record<string, any>, stepOrder = 1) {
+    const records = [];
+    // Base Record
+    records.push(this.keyValue('registrant', 'Checked Registrant', registrant));
+    // Ordered Record
+    records.push(this.keyValue(`registrant.${stepOrder}`, `Created Registrant from Step ${stepOrder}`, registrant));
+    return records;
   }
 }
 
